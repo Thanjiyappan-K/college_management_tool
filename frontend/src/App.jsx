2528d88688719f97587fb9c7a481d549d3197485
22009011 <thanjiyappankanniyappan@gmail.com>
2025-03-25 12:41:24 +0530

    student

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Login from './Login.jsx'
 import Register from './Register'
 import './App.css' 
 import CollegeAdminDashboard from './Components/Admin/CollegeAdminDashboard.jsx'
+import StudentDashboard from './Components/Student/StudentDashboard.jsx'
 import Adduser from './Components/Admin/Adduser.jsx'
 import TeacherDashboard from './Components/Teacher/TeacherDashboard.jsx'
 import Home from './Components/Home/Home.jsx'
@@ -21,6 +22,8 @@ function App() {
         <Route path="/admin" element={<CollegeAdminDashboard />} />
         <Route path="/admin/user" element={<Adduser/>} />
         <Route path="/teacher" element={<TeacherDashboard />} />
+        <Route path="/student" element={<StudentDashboard />} />
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </BrowserRouter>
     </>
